6c16b977d048102d8cce16bfbb9fa2849996e764
Jouella Fabe <jojofabe@vt.edu>
2014-05-12 06:28:27 -0400

    added more functionalities for the web api service

--- main.go
+++ main.go
@@ -11,54 +11,116 @@ import (
   "fmt"
 )
 
-//Data model
 type Person struct {
+  UserName string
   Name string
   Age int
+  Password string
+  EmailAddress string
+}
+var personTemp Person
+var manager PeopleManager
+
+//a collection array that stores the list of people
+type PeopleManager struct {
+  peopleManager map[string]*Person
 }
 
-var ryanne Person
 
-func ryanneHandler(w http.ResponseWriter, req *http.Request) {
+func peopleHandler(w http.ResponseWriter, req *http.Request) {
   fmt.Println(req.URL.Path)
+
   switch req.Method {
     case "GET":
-      buf, _ := json.Marshal(&ryanne)
+
+      buf, _ := json.Marshal(&manager)
       w.Write(buf)
 
     //curl -v -H "Content-Type: application/json" -X PUT --data "stuff.json" http://localhost:8003/ryanne
     //this uploads
     case "PUT":
       buf, _ := ioutil.ReadAll(req.Body)
-      err := json.Unmarshal(buf, &ryanne)
-      if err != nil {
-        fmt.Println("error: ", err)
-      }
-      fmt.Printf("%s %d", ryanne.Name, ryanne.Age)
+      err := json.Unmarshal(buf, &personTemp)
+      if printErr (&err) == 1{
+        fmt.Printf("%s %d", personTemp.Name, personTemp.Age)
+
+    }
+
+    case "DELETE":
+      buf, _ := ioutil.ReadAll(req.Body)
+      err := json.Unmarshal(buf, &personTemp)
+      printErr (&err)
+
+
+    case "POST":
+      //to-do stub
 
     default:
       w.WriteHeader(400)
   }
 }
 
+//Handles each specific person that is in the peopleHandler
+func personHandler(w http.ResponseWriter, req *http.Request){
 
-func handler(w http.ResponseWriter, req *http.Request){
-  switch req.URL {
-    case "/register":
-      //go to this function handler
-      //and handle the registration process
-    case "/login":
+    exists := isPersonExists(req.URL.Path[1:])
+    person := req.URL.Path[1:]
+    switch req.Method{
+      case "GET":
 
+        if exists {
+          buf, _ := json.Marshal(&personTemp)
+          w.Write(buf)
+        }
+        else {
+          w.Write("couldn't find ", person)
+        }
 
+      case "PUT":
+        if exists {
+          buf, err := json.Unmarshal(buf, manager[&person])
+        }
+        printErr (&err)
+      case "DELETE":
+        if exists {
+          //delete the resource off the server
+        }
+
+      case "POST":
+        if !exists {
+          //
+        }
+
+      default:
+        w.WriteHeader(400) //not found code
+
+}
+
+func printErr (err *string) error{
+  if &err != nil{
+    fmt.Println("error: ", &err)
+    return nil
+  }
+  else return 1
+}
+
+func isPersonExists(person *string) error boolean {
+  if val, ok := manager[&person]; ok {
+    return true
+  }
+  else {
+    return false
   }
 }
 
-
 func main() {
-  ryanne.Name = "Ryanne"
-  ryanne.Age = 25
 
-  http.HandleFunc("/ryanne", ryanneHandler)
+  //starting variables
+  manager.peopleManager = make(map[string]*Person)
+  //personTemp.UserName = ""
+
+  http.HandleFunc("/people", peopleHandler)
+  http.HandleFunc("/{people}", personHandler)
 
   http.ListenAndServe(":8003", nil)
 }
